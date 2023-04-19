f3c0bbad0aa2ca00f994fc555fa68d2750b8dc96
modernxpunk <modernxpunk@gmail.com>
2023-04-19 20:20:25 +0300

    cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,9 +25,19 @@ module.exports = {
 			xl: "1280px",
 			"2xl": "1536px",
 		},
+		keyframes: {
+			appearence: {
+				"0%": { transform: "scale(0.8)", opacity: 0 },
+				"100%": { transform: "scale(1)", opacity: 1 },
+			},
+		},
+		animation: {
+			appear: "appearence 0.2s linear",
+		},
 		extend: {
 			fontFamily: {
 				sans: ["var(--font-roboto)", ...fontFamily.sans],
+				title: ["var(--font-montserrat)", ...fontFamily.serif],
 			},
 		},
 	},
