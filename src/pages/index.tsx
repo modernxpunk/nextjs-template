a46cf035ed3a20f6f764d5a0c04c1e074b8c92db
modernxpunk <modernxpunk@gmail.com>
2023-04-19 20:24:50 +0300

    add font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 import { Button } from "@ui/Button";
+import { Modal } from "@ui/Modal";
+import { useState } from "react";
 import Layout from "src/components/common/Layout";
 import { NextPageWithLayout } from "src/types/common";
 
 const Home: NextPageWithLayout = () => {
+	const [isOpen, setIsOpen] = useState<boolean>(false);
 	return (
 		<div className="container">
-			<Button>hello world</Button>
+			<Button onClick={() => setIsOpen(true)}>open modal</Button>
+			<Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+				Hello world
+			</Modal>
+			<h3 className="text-2xl font-title font-bold">Hello world</h3>
 		</div>
 	);
 };
