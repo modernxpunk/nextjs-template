81d9c9470e74e71347ad76a01311bb1d75d40b61
OleksandrKovalskyi <modernxpunk@gmail.com>
2023-12-11 05:46:20 +0200

    own design system

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import Layout from "@/components/layout";
+import { Button } from "@/components/ui/Button";
 import { NextPageWithLayout } from "@/types/common";
 
 const Home: NextPageWithLayout = () => {
-	return <div className="container"></div>;
+	return (
+		<div className="container">
+			<Button>asdf</Button>
+			<Button intent="secondary">asdf</Button>
+		</div>
+	);
 };
 
 Home.getLayout = (page) => {
