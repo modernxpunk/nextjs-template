df0601b346d5b823fbcb0bde053662f59877da63
OleksandrKovalskyi <modernxpunk@gmail.com>
2023-12-16 19:19:47 +0200

    add theme changer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import { Montserrat, Roboto } from "next/font/google";
+import Icon from "@/components/ui/icon";
 
 const roboto = Roboto({
 	subsets: ["latin"],
@@ -26,11 +27,26 @@ const Layout = ({ children }: Props) => {
 				.join(",")
 				.replaceAll(",", " ")} font-sans`}
 		>
-			<header>
-				<nav></nav>
+			<header className="py-3 bg-base-200">
+				<nav className="container flex items-center justify-between">
+					<div>
+						<h1>App</h1>
+					</div>
+					<div>
+						<label className="btn btn-sm btn-ghost btn-circle swap swap-rotate">
+							<input
+								type="checkbox"
+								className="theme-controller"
+								value="light"
+							/>
+							<Icon className="text-2xl swap-on" name="common/sun" />
+							<Icon className="text-2xl swap-off" name="common/moon" />
+						</label>
+					</div>
+				</nav>
 			</header>
-			<main className="flex-1">{children}</main>
-			<footer></footer>
+			<main className="flex flex-col flex-1 py-3">{children}</main>
+			<footer className="py-3 text-center bg-base-200">Made with love</footer>
 		</div>
 	);
 };
