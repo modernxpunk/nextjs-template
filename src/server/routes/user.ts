1e8e2f4503e81a80f12732930337e43a612cc6a2
OleksandrKovalskyi <modernxpunk@gmail.com>
2023-12-27 00:04:14 +0200

    add example of route in openapi

--- src/server/routes/user.ts
+++ src/server/routes/user.ts
@@ -1,30 +1,39 @@
 import { protectedProcedure, publicProcedure, router } from "@/server/trpc";
 import { eq } from "drizzle-orm";
 import { db } from "drizzle/db";
-import { UserTable, insertUserSchema, selectUserSchema } from "drizzle/schema";
+import { users, insertUserSchema, selectUserSchema } from "drizzle/schema";
 import z from "zod";
 
 const userRouter = router({
 	getAll: protectedProcedure
-		.meta({ /* 👉 */ openapi: { method: "GET", path: "/get-all" } })
+		.meta({
+			openapi: {
+				method: "GET",
+				description: "description",
+				path: "/get-all",
+				tags: ["users"],
+				summary: "Summary",
+				protect: true,
+			},
+		})
 		.input(z.undefined())
 		.output(selectUserSchema.array())
 		.query(async () => {
-			const users = await db.query.UserTable.findMany();
+			const users = await db.query.users.findMany();
 			return users;
 		}),
 
 	getById: publicProcedure
 		.input(selectUserSchema.pick({ id: true }))
 		.query(async (opts) => {
-			const user = await db.query.UserTable.findFirst({
-				where: eq(UserTable.id, opts.input.id),
+			const user = await db.query.users.findFirst({
+				where: eq(users.id, opts.input.id),
 			});
 			return user;
 		}),
 
 	createUser: publicProcedure.input(insertUserSchema).mutation(async (opts) => {
-		const user = await db.insert(UserTable).values(opts.input);
+		const user = await db.insert(users).values(opts.input);
 		return user;
 	}),
 
@@ -37,9 +46,9 @@ const userRouter = router({
 		)
 		.mutation(async (opts) => {
 			const editedUsers = await db
-				.update(UserTable)
+				.update(users)
 				.set(opts.input.data)
-				.where(eq(UserTable.id, +opts.input.id))
+				.where(eq(users.id, +opts.input.id))
 				.returning();
 			const editedUser = editedUsers[0];
 			return editedUser;
@@ -49,8 +58,8 @@ const userRouter = router({
 		.input(selectUserSchema.pick({ id: true }))
 		.mutation(async (opts) => {
 			const deletedUsers = await db
-				.delete(UserTable)
-				.where(eq(UserTable.id, opts.input.id))
+				.delete(users)
+				.where(eq(users.id, opts.input.id))
 				.returning();
 			const deletedUser = deletedUsers[0];
 			return deletedUser;
