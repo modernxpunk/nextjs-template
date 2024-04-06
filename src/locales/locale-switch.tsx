e5678d0897f97658a85c78f59c39c4b0bbda937c
OleksandrKovalskyi <modernxpunk@gmail.com>
2024-04-06 04:57:14 +0300

    fix dropdown

--- src/locales/locale-switch.tsx
+++ src/locales/locale-switch.tsx
@@ -2,29 +2,13 @@
 
 import Icon from "@/components/icon";
 import { useChangeLocale, useI18n } from "@/locales/client";
-import { useRef, useEffect } from "react";
+import { useRef } from "react";
 
 const LocaleSwitcher = () => {
 	const t = useI18n();
 
-	// const locale = useCurrentLocale();
 	const changeLocale = useChangeLocale({ preserveSearchParams: true });
 	const detailsRef = useRef<HTMLDetailsElement | null>(null);
-
-	useEffect(() => {
-		const handleClickOutside = (event: MouseEvent) => {
-			if (
-				detailsRef.current &&
-				!detailsRef.current.contains(event.target as Node)
-			) {
-				detailsRef.current.open = false;
-			}
-		};
-		document.addEventListener("mousedown", handleClickOutside);
-		return () => {
-			document.removeEventListener("mousedown", handleClickOutside);
-		};
-	}, []);
 
 	return (
 		<details ref={detailsRef} className="dropdown dropdown-end">
@@ -33,10 +17,22 @@ const LocaleSwitcher = () => {
 				<Icon name="common/chevron-down" />
 			</summary>
 			<div className="p-2 shadow menu mt-4 dropdown-content z-[1] bg-base-200 rounded-box w-52">
-				<button className="btn" onClick={() => changeLocale("en")}>
+				<button
+					className="btn"
+					onClick={() => {
+						changeLocale("en");
+						detailsRef.current && (detailsRef.current.open = false);
+					}}
+				>
 					{t("languages.en")}
 				</button>
-				<button className="btn" onClick={() => changeLocale("ua")}>
+				<button
+					className="btn"
+					onClick={() => {
+						changeLocale("ua");
+						detailsRef.current && (detailsRef.current.open = false);
+					}}
+				>
 					{t("languages.ua")}
 				</button>
 			</div>
