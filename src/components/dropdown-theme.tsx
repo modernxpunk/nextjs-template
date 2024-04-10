7483fc658d3d1784baeee34087cb0513061276fe
OleksandrKovalskyi <modernxpunk@gmail.com>
2024-04-10 14:24:03 +0300

    fix toggle theme

--- src/components/dropdown-theme.tsx
+++ src/components/dropdown-theme.tsx
@@ -4,18 +4,23 @@ import Icon from "@/components/icon";
 import { useTheme } from "next-themes";
 
 const DropdownTheme = () => {
-	const { theme, setTheme } = useTheme();
+	const { resolvedTheme, setTheme } = useTheme();
+
+	const toggleTheme = () => {
+		setTheme(resolvedTheme === "dark" ? "light" : "dark");
+	};
 
 	return (
 		<label className="swap btn btn-circle btn-ghost swap-rotate">
 			<input
 				type="checkbox"
 				className="theme-controller"
-				onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
-				checked={theme === "dark"}
+				onChange={toggleTheme}
+				value={resolvedTheme}
+				checked={resolvedTheme === "dark"}
 			/>
-			<Icon className="text-2xl swap-off" name="common/white-balance-sunny" />
-			<Icon className="text-2xl swap-on" name="common/moon-waning-crescent" />
+			<Icon className="text-2xl swap-on" name="common/white-balance-sunny" />
+			<Icon className="text-2xl swap-off" name="common/moon-waning-crescent" />
 		</label>
 	);
 };
