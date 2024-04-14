45811e7ec438282b11aa6bc74217a71b0e3d6df4
OleksandrKovalskyi <modernxpunk@gmail.com>
2024-04-14 18:05:55 +0300

    delete files

--- src/locales/server.ts
+++
@@ -1,7 +0,0 @@
-// locales/server.ts
-import { createI18nServer } from "next-international/server";
-
-export const { getI18n, getScopedI18n, getStaticParams } = createI18nServer({
-	en: () => import("./directories/en"),
-	ua: () => import("./directories/ua"),
-});
