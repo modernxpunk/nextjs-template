f19e02136d536981f746d23b324057c167b4a237
OleksandrKovalskyi <modernxpunk@gmail.com>
2024-04-14 15:04:59 +0300

    delete

--- src/app/[locale]/client/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { I18nProviderClient } from "@/locales/client";
-import { ReactElement } from "react";
-
-export default function SubLayout({ children }: { children: ReactElement }) {
-	return <I18nProviderClient locale="en">{children}</I18nProviderClient>;
-}
