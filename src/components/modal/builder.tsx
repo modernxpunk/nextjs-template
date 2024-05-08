72e8428726eda3dcc65e4681b7d3f45981437093
OleksandrKovalskyi <modernxpunk@gmail.com>
2024-05-08 14:10:24 +0300

    add modal template

--- src/components/modal/builder.tsx
+++ src/components/modal/builder.tsx
@@ -1,10 +1,12 @@
 import ModalContainer from "@/components/modal/container";
 import Modal1 from "@/components/modal/content/modal1";
 import Modal2 from "@/components/modal/content/modal2";
+// New import
 
 const modals = [
 	{ id: "add_cat", Modal: Modal1 },
 	{ id: "example", Modal: Modal2 },
+	// New modal
 ];
 
 const ModalBuilder = () => {
