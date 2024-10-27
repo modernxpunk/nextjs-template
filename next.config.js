9c6650e64702e377a75a4acfffcb59f99cff1db2
OleksandrKovalskyi <modernxpunk@gmail.com>
2024-10-27 16:21:55 +0200

    del i18n library

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
-const withNextIntl = require("next-intl/plugin")("./src/lib/i18n/config.ts");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
-	images: {
-		domains: ["picsum.photos"],
-	},
 };
 
-module.exports = withNextIntl(nextConfig);
+module.exports = nextConfig;
