95326e3ede8d30015c5ad64be0cea7f87ec0a3f5
OleksandrKovalskyi <modernxpunk@gmail.com>
2025-03-29 03:56:35 +0200

    format

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 const createNextIntlPlugin = require("next-intl/plugin");
 
-const withNextIntl = createNextIntlPlugin(
-	"./src/lib/i18n/request.ts"
-);
+const withNextIntl = createNextIntlPlugin("./src/lib/i18n/request.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	"output": "standalone",
+	output: "standalone",
 };
 
 module.exports = withNextIntl(nextConfig);
